938978cd2a448b1190511896a66116dd5431c918
HeIIoween <primalfear@pochtamt.ru>
2024-04-03 00:18:54 +0300

    Update 3_Играть_Play.cmd

--- 3_Играть_Play.cmd
+++ 3_Играть_Play.cmd
@@ -1,3 +1,9 @@
+@echo off 
+echo GITHub mozhet tormozit'. Ozhidayte...
+echo I ne trogay'te faily...! :-E
+echo GITHub might be unresponsible - just wait... Zzz...
+echo And do not touch the files! :-E
+.\Misc\PortableGit\bin\git restore .
 .\Misc\PortableGit\bin\git pull https://github.com/HeIIoween/opensirius.git
 
 taskkill /IM freelancer.bin /F
